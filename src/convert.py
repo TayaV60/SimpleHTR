bada17fcd94951d7813d82203f0a1ff161bc2380
Taisiya Velarde <tv192@student.aru.ac.uk>
2021-07-04 22:38:08 +0100

    WIP convert json image metadata to words.txt format

--- src/convert.py
+++ src/convert.py
@@ -2,7 +2,7 @@ import argparse
 from path import Path
 import json
 import os
-import datetime
+import glob
 
 class ConverstionFilePaths:
     """Filenames and paths to data."""
@@ -11,16 +11,30 @@ class ConverstionFilePaths:
     image_extension = 'jpg'
 
 def iterate_json_files(directory):
-  for filename in os.listdir(directory):
-    if filename.endswith(".json"): 
-        filepath = os.path.join(directory, filename)
+  image_data_list = []
+  for filepath in sorted(glob.glob(directory + '/*.json')):
+    if filepath.endswith(".json"): 
+        filepath = os.path.join(filepath)
         with open(filepath) as jsonFile:
           data = json.load(jsonFile)
+          filename = os.path.basename(filepath)
           file_without_extension = os.path.splitext(filename)[0]
-          print(file_without_extension + " " + str(data["size"]["width"]) + " " + str(data["size"]["height"])  + " " + data["description"])
-        continue
-    else:
-        continue
+          image_data = {
+            "id": file_without_extension,
+            "width": data["size"]["width"],
+            "height": data["size"]["height"],
+            "description": data["description"]
+          }
+          image_data_list.append(image_data)
+          # print(file_without_extension + " ok 154 0 0 " + str(data["size"]["width"]) + " " + str(data["size"]["height"])  + " " + data["description"])
+  return image_data_list
+
+def save_words_txt(image_data_list, destination_folder):
+  with open(destination_folder + '/words.txt', 'w') as f:
+    for item in image_data_list:
+        line = item["id"] + " ok 154 0 0 " + str(item["width"]) + " " + str(item["height"]) + " " + item["description"]
+        f.write("%s\n" % line)
+
 
 def main():
     """Main conversion function (converts Russian dataset to IAM format)."""
@@ -36,17 +50,17 @@ def main():
     assert args.original_folder.exists()
     assert args.destination_folder.exists()
 
+    print(args.original_folder + '/' + ConverstionFilePaths.annotation_files)
+
     annotation_files_folder = Path(args.original_folder + '/' + ConverstionFilePaths.annotation_files)
     image_files_folder =  Path(args.original_folder + '/' + ConverstionFilePaths.image_files)
 
     assert annotation_files_folder.exists()
     assert image_files_folder.exists()
 
-    iterate_json_files(annotation_files_folder)
+    image_data_list = iterate_json_files(args.original_folder + '/' + ConverstionFilePaths.annotation_files)
+    save_words_txt(image_data_list, args.destination_folder)
+
 
 if __name__ == '__main__':
-    print("STARTED")
-    print(datetime.datetime.now())
     main()
-    print("ENDED")
-    print(datetime.datetime.now())
